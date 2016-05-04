8e1efd9e066dfc73fe6f377171936740e9a24d5e
deathcap <deathcap@gmx.co.uk>
2016-05-03 21:18:22 -0700

    Wrap all modules in a closure to fix global leak. It is the only way, with current web technologies.

--- cjs4web.js
+++ cjs4web.js
@@ -15,19 +15,48 @@ const root = process.argv[2] || '.';
 const node_modules = path.join(root, 'node_modules');
 const outRoot = path.join(root, 'build');
 
-fs.readdir(node_modules, (err, dirs) => {
+// TODO: switch to promises
+fs.mkdir(outRoot, (err) => {
+  //if (err) throw err;
 
-  for (let i = 0; i < dirs.length; ++i) {
-    const dir = path.join(node_modules, dirs[i]);
+  fs.mkdir(path.join(outRoot, 'node_modules'), (err) => {
+    //if (err) throw err;
 
-    const package_json = path.join(dir, 'package.json');
+    fs.readdir(node_modules, (err, dirs) => {
+      if (err) throw err;
 
-    fs.readFile(package_json, 'utf8', (err, jsonData) => {
-      const json = JSON.parse(jsonData);
+      for (let i = 0; i < dirs.length; ++i) {
+        const dir = path.join(node_modules, dirs[i]);
+        const package_json = path.join(dir, 'package.json');
 
-      const main = path.join(dir, json['main'] || 'index.js');
+        fs.readFile(package_json, 'utf8', (err, jsonData) => {
+          if (err) throw err;
 
-      console.log(main);
+          const json = JSON.parse(jsonData);
+          const main = path.join(dir, json['main'] || 'index.js');
+
+          fs.readFile(main, 'utf8', (err, data) => {
+            if (err) throw err;
+
+            const outDir = path.join(outRoot, dir);
+            fs.mkdir(outDir, (err) => {
+              //if (err) throw err;
+
+              const outFile = path.join(outRoot, main);
+
+              // Wrap all modules in a closure
+              const exposedVariables = 'require,exports,module';
+              const newData = `((${exposedVariables}) => {${data}\n})(${exposedVariables});`;
+
+              fs.writeFile(outFile, newData, (err) => {
+                if (err) throw err;
+              });
+            });
+          });
+
+          console.log(main);
+        });
+      }
     });
-  }
+  });
 });
